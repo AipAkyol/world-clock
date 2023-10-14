d6610f49f51492ba9ee38a0c11d0971cb15daa75
Alperen Akyol <a-akyol@hotmail.com>
2023-10-14 23:59:21 +0300

    damn

--- src/lib/getTime.js
+++ src/lib/getTime.js
@@ -1,8 +1,8 @@
 import timezones from "$lib/data/timezones.json";
-export default function getTime(city){
+export default function getTime(continent, city){
     const date = new Date();
     const localOffset = (-1) * (date.getTimezoneOffset()/60);
-    const timeOffset = timezones.europe[city];
+    const timeOffset = timezones[continent][city];
     date.setHours(date.getHours()-localOffset+timeOffset);
     return date;
 }
